e089eae2a737134b3771cc18e37ece91f15c7cab
Johan B <davidbm270@gmail.com>
2025-05-20 08:10:19 -0500

    Se instalaron librerias para poder abrir el archivo

--- gestion_contratos/settings.py
+++ gestion_contratos/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'gestion_contratos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion_contratos',
-        'USER': 'root',
-        'PASSWORD': '00000',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'gestion_contratos',
+#         'USER': 'root',
+#         'PASSWORD': '00000',
+#         'HOST': 'localhost',
+#         'PORT': '3307',
+#     }
+# }
 
 
 # Password validation
